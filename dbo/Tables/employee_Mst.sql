f498a3a73919a2e6e1a0c168e7ae3687909be052
BhalchandraDesai <Bhalchandra.Desai@assetsoft.biz>
2024-06-20 13:04:07 +0530

    Created By Added

--- dbo/Tables/employee_Mst.sql
+++ dbo/Tables/employee_Mst.sql
@@ -2,6 +2,7 @@
     [ID]       INT           IDENTITY (1, 1) NOT NULL,
     [Emp_Name] VARCHAR (100) NULL,
     [Emp_Add]  VARCHAR (500) NULL, 
-    [CreatedDate] DATETIME NULL
+    [CreatedDate] DATETIME NULL, 
+    [Created By] VARCHAR(50) NULL
 );
 
